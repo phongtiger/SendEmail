2fcaeca0ecfaa75e758d9783a5980b5550f862a1
phongtv <phongtv@ttc-solution.com>
2021-04-06 15:28:31 +0700

    Test done send email

---
+++ src/main/java/com/email/demo/config/Configs.java
@@ -0,0 +1,13 @@
+package com.email.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class Configs {
+
+}
