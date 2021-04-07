954c6d86a4f48c55658b750e22f14b08d5df8366
phongtv <phongtv@ttc-solution.com>
2021-04-07 16:47:32 +0700

    Send email đính kèm ảnh

--- src/main/java/com/email/demo/SimpleEmailExampleController.java
+++ src/main/java/com/email/demo/SimpleEmailExampleController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
+import org.springframework.mail.MailParseException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -15,12 +16,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeUtility;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
 
 @Controller
 public class SimpleEmailExampleController {
@@ -46,40 +50,20 @@ public class SimpleEmailExampleController {
     @ResponseBody
     @RequestMapping("/sendSimpleEmail-with-attract")
     public String sendMailWithAttachment(String fileToAttach) throws MessagingException {
-        String mailTo = "phongtv@ttc-solutions.com.vn";
         MimeMessage message = mailSender.createMimeMessage();
+        String mailTo = "phongtv@ttc-solutions.com.vn";
 
-        // use the true flag to indicate you need a multipart message
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setTo(mailTo);
+        try{
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(mailTo);
+            helper.setSubject("sdas");
+            helper.setText("hahsasd");
+            FileSystemResource file = new FileSystemResource("C:\\Users\\phongtv\\Desktop\\Talens Genera (1).png");
+            helper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
 
-        helper.setSubject("Email with Inline images Example");
-        helper.setText(
-                "<html>"
-                        + "<body>"
-                        + "<div>Dear student,"
-                        + "<div><strong>Add the image to the right:</strong></div>"
-                        + "<div>"
-                        + "<img src='cid:rightSideImage' style='float:right;width:50px;height:50px;'/>"
-                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
-                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
-                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
-                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
-                        + "</div>"
-                        + "<div><strong>Add the image to the left :</strong></div>"  + "<div>"
-                        + "<img src='cid:leftSideImage' style='float:left;width:50px;height:50px;'/>"
-                        + "<div>Adding a inline resource/image on to the left of the paragraph.</div>"
-                        + "<div>Adding a inline resource/image on to the left of the paragraph.</div>"
-                        + "<div>Adding a inline resource/image on to the left of the paragraph.</div>"
-                        + "<div>Adding a inline resource/image on to the left of the paragraph.</div>"
-                        + "</div>"
-                        + "<div>Thanks,</div>"
-                        + "kalliphant"
-                        + "</div></body>"
-                        + "</html>", true);
-        helper.addInline("rightSideImage",
-                new File("C:/Users/phongtv/Desktop/Talens Genera (1).png"));
-
+        }catch (MessagingException e) {
+            throw new MailParseException(e);
+        }
         mailSender.send(message);
         return "thanh cong";
     }
