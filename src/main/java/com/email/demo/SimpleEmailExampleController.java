6d7a214379baa997ed842e765dddfedad6fcf043
phongtv <phongtv@ttc-solution.com>
2021-04-07 15:36:27 +0700

    gửi email với file txt

--- src/main/java/com/email/demo/SimpleEmailExampleController.java
+++ src/main/java/com/email/demo/SimpleEmailExampleController.java
@@ -1,11 +1,24 @@
 package com.email.demo;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import java.io.File;
+
 @Controller
 public class SimpleEmailExampleController {
     @Autowired
@@ -26,5 +39,38 @@ public class SimpleEmailExampleController {
         this.emailSender.send(message);
 
         return "Email Sent!";
+    }
+    @ResponseBody
+    @RequestMapping("/sendSimpleEmail-with-attract")
+    public String sendMailWithAttachment(String fileToAttach){
+        String to = "phongtv@ttc-solutions.com.vn";
+        String subject = "Test Simple Email attachment";
+        String body = "Hello, Im testing Simple Email";
+        MimeMessagePreparator preparator = new MimeMessagePreparator()
+        {
+            public void prepare(MimeMessage mimeMessage) throws Exception
+            {
+                mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                mimeMessage.setFrom(new InternetAddress("admin@gmail.com"));
+                mimeMessage.setSubject(subject);
+                mimeMessage.setText(body);
+                File file = new File("C:/Users/phongtv/Desktop/new 3.txt");
+                System.out.println("Path exists? " + file.exists());
+                FileSystemResource file2 = new FileSystemResource(file);
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+                helper.addAttachment("new 3.txt", file2);
+                helper.setText("", true);
+            }
+        };
+
+        try {
+            this.emailSender.send(preparator);
+            return "thanh cong";
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+            return ex.getMessage();
+        }
     }
 }
